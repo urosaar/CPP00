c4c85fd9708c159f2ae6d66f8f5e0ff9398edfd3
urosaar <oussra061@gmail.com>
2025-11-21 16:30:52 +0100

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-using namespace std;
 
-int main ()
+int main (int argc, char **argv)
 {
-  cout << "ktb chi haj!";
-  return 0;
+  if (argc == 1)
+  {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return 0;
+  }
+  for (int i = 1; argv[i]; i++)
+  {
+    for (int j = 0; argv[i][j]; j++)
+    {
+      argv[i][j] = toupper(argv[i][j]);
+      std::cout << argv[i][j];
+    }
+  }
+  std::cout << std::endl;
 }
